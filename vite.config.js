9f92317d476d38c3bc4f83b279460571461dd70a
Diya030805 <169034790+student-diyaghosh@users.noreply.github.com>
2025-10-02 20:19:45 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  base:"my-protfolio/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
